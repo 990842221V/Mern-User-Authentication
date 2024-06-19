a0c4a697c9d8e55314ac3fd740db6b052b9a7ec9
990842221V <shashikakalhara24@gmail.com>
2024-06-19 21:53:24 +0530

    Update auth.routes.js

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,7 +3,10 @@ import { login, logout, signup } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
+//Signup Rout
 router.post('/signup', signup)
+
+//Login Rout
 router.post('/login', login)
 router.post('/logout', logout)
 
